c17d73d8def30bd5353b2db267767ff00f3c8ace
Len Budney <Len.Budney@grantstreet.com>
2021-07-18 22:43:20 -0400

    debug -> info

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def page_unauthorized(e):
 
 def main(request):
   logging.info("Update request started.")
-  logging.debug(request.__dict__)
+  logging.info(request.__dict__)
   query_parameters = request.args
   
   # Assign our parameters
